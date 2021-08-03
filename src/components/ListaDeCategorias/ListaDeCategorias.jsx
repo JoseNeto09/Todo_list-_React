7e343cdb1ddea1b24ca83711343a1a887e26a9d0
José lopes <joselopessobrinhoneto9@gmail.com>
2021-08-03 15:20:18 -0300

    iniciando projeto

---
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -0,0 +1,10 @@
+import  React, { Component } from 'react';
+
+class ListaDeCategorias extends Component {
+    state = {  }
+    render() { ListaDeCategorias
+        return (  );
+    }
+}
+ 
+export default ;
